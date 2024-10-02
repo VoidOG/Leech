043fd0078c570230f613952422231913ac7a611c
VoidOG <VoidGod123@outlook.com>
2024-10-02 17:01:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-BOT_TOKEN = ""
-LOG_GROUP_ID = ""
+BOT_TOKEN = "7612839684:AAHNlWYvhL2x_Gc3u5_H-2GEESpfIOe6Ql8"
+LOG_GROUP_ID = "-1002183841044"
 UPLOAD_GROUPS = [
-    "",
+    "-1002183841044",
 ]
 
 AUTHORIZED_USERS = [
-    "",
-    "",
+    "6663845789",
+    "6698364560",
 ]
 VPS_LOG_FILE = "/var/log/Leech/logs.txt"
