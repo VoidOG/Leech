9ecf0b81822a95344cea6fe6aec431137ffbc067
VoidOG <VoidGod123@outlook.com>
2024-10-02 15:14:37 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+BOT_TOKEN = 'YOUR_BOT_TOKEN'
+LOG_GROUP_ID = 'YOUR_LOG_GROUP_ID'
+
+# List of channel/group IDs where the bot will upload files
+UPLOAD_GROUPS = [
+    'GROUP_OR_CHANNEL_ID_1',
+    'GROUP_OR_CHANNEL_ID_2',
+    'GROUP_OR_CHANNEL_ID_3',
+    # Add more group or channel IDs as needed
+]
+
+# List of authorized user IDs
+AUTHORIZED_USERS = [
+    'USER_ID_1',
+    'USER_ID_2',
+    # Add more user IDs as needed
+]
+
+# Path to the VPS log file
+VPS_LOG_FILE = '/path/to/vps/log/file.log'  # Update this path accordingly
