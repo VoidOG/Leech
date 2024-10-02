2afb467160cf2d1124be3022f03d4bf847e2c6ef
VoidOG <VoidGod123@outlook.com>
2024-10-02 16:58:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,11 @@
-BOT_TOKEN = 'YOUR_BOT_TOKEN'
-LOG_GROUP_ID = 'YOUR_LOG_GROUP_ID'
-
-# List of channel/group IDs where the bot will upload files
+BOT_TOKEN = ''
+LOG_GROUP_ID = ''
 UPLOAD_GROUPS = [
-    'GROUP_OR_CHANNEL_ID_1',
-    'GROUP_OR_CHANNEL_ID_2',
-    'GROUP_OR_CHANNEL_ID_3',
-    # Add more group or channel IDs as needed
+    ''
 ]
 
-# List of authorized user IDs
 AUTHORIZED_USERS = [
-    'USER_ID_1',
-    'USER_ID_2',
-    # Add more user IDs as needed
+    '',
+    '',
 ]
-
-# Path to the VPS log file
-VPS_LOG_FILE = '/var/log/Leech/logs.txt'  # Update this path accordingly
+VPS_LOG_FILE = '/var/log/Leech/logs.txt'
